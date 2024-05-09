c1ad67f002da4c040f79dc118ecef1042d874421
Mark Salvatore <salvatore.mark@gmail.com>
2024-05-09 12:55:51 -0400

    update namespace

--- text-slicer.php
+++ text-slicer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:       Text Slicer
  * Description:       Applies a slice effect to text.
@@ -8,24 +9,24 @@
  * Author:            Mark Salvatore
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       smark
+ * Text Domain:       text-slicer
  */
 
- defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
- if ( ! class_exists( 'Text_Slicer' ) ) {
-   class Text_Slicer {
-     function __construct() {
-        add_action( 'init', array( $this, 'block_init' ) );
-     }
- 
-     /**
-      * Register the block using the metadata loaded from `block.json`.
-      * It also registers all assets so they can be enqueued.
-      */
-     function block_init() {
-        register_block_type( __DIR__ . '/build' );
-      }
+if (!class_exists('Text_Slicer')) {
+  class Text_Slicer {
+    function __construct() {
+      add_action('init', array($this, 'block_init'));
     }
- }
- $blockSlicer = new Text_Slicer();
+
+    /**
+     * Register the block using the metadata loaded from `block.json`.
+     * It also registers all assets so they can be enqueued.
+     */
+    function block_init() {
+      register_block_type(__DIR__ . '/build');
+    }
+  }
+}
+$blockSlicer = new Text_Slicer();
